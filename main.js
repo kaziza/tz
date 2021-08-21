20aff77d39eff88cbf27125e3d1cf5afc1828079
Хостел Красная Москва <hostelkrasnaamoskva@iMac.local>
2021-08-21 12:03:02 +0300

    animation on add/delete, item hover

--- main.js
+++ main.js
@@ -5,89 +5,22 @@ new Vue({
 		newItemName: '',
 		newItemDesc: '',
 		newItemImg: '',
-		newItemPrice: 0,
+		newItemPrice: '',
 		formValid: (this.newItemName && this.newItemImg),
 		sortBy: 'default',
 		items: [
-			{
-				name: 'ccccccc',
-				desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-				img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-				price: 300000
-			},
-			{
+		{
+			name: 'ccccccc',
+			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
+			price: 300000
+		},
+		{
 			name: 'aaaa',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
 			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
 			price: 10000
-		},
-		{
-			name: 'bbbb',
-			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-			price: 200000
-		},
-		
-		// {
-		// 	name: 'Наименование товара',
-		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-		// 	price: 10000
-		// },
-		// {
-		// 	name: 'Наименование товара',
-		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-		// 	price: 10000
-		// },
-		// {
-		// 	name: 'Наименование товара',
-		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-		// 	price: 10000
-		// },
-		// {
-		// 	name: 'Наименование товара',
-		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-		// 	price: 10000
-		// }
-		],
-		computed: {
-			sortedByMin: function() {
-			  function compare(a, b) {
-				if (a.price < b.price)
-				  return -1;
-				if (a.price > b.price)
-				  return 1;
-				return 0;
-			  }
-		
-			  return this.items.sort(compare);
-			},
-			sortedByMax: function() {
-				function compare(a, b) {
-				  if (a.price > b.price)
-					return -1;
-				  if (a.price < b.price)
-					return 1;
-				  return 0;
-				}
-		  
-				return this.items.sort(compare);
-			},
-			sortedByName: function() {
-				function compare(a, b) {
-				  if (a.name < b.name)
-					return -1;
-				  if (a.name > b.name)
-					return 1;
-				  return 0;
-				}
-		  
-				return this.items.sort(compare);
-			}
-		},
+		}],
 		sortOptions: [
 			{ value: 'default', text: 'По умолчанию' },
 			{ value: 'byMin', text: 'По цене min (от меньшего к большему)' },
@@ -101,13 +34,13 @@ new Vue({
 			this.items.push({
 				name: this.newItemName,
 				desc: this.newItemDesc,
-				img: this.newItemImg || "https://www.goancuisine.com.au/wp-content/uploads/2017/05/holdee.jpg",
-				price: this.newItemPrice || 0
+				img: this.newItemImg,
+				price: this.newItemPrice || ''
 			})
 			this.newItemName = '',
 			this.newItemDesc = '',
 			this.newItemImg = '',
-			this.newItemPrice = 0
+			this.newItemPrice = ''
 		},
 		removeItem(index) {
 			this.items.splice(index, 1)
