2f3cefb2b8700f13ee5c87b2bb0a359efd1e071b
Хостел Красная Москва <hostelkrasnaamoskva@iMac.local>
2021-08-21 10:19:22 +0300

    select css, sort by name, price(min, max)

--- main.js
+++ main.js
@@ -7,48 +7,93 @@ new Vue({
 		newItemImg: '',
 		newItemPrice: 0,
 		formValid: (this.newItemName && this.newItemImg),
-		items: [{
-			name: 'Наименование товара',
+		sortBy: 'default',
+		items: [
+			{
+				name: 'ccccccc',
+				desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+				img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
+				price: 300000
+			},
+			{
+			name: 'aaaa',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
 			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
 			price: 10000
 		},
 		{
-			name: 'Наименование товара',
+			name: 'bbbb',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
 			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-			price: 10000
+			price: 200000
 		},
-		{
-			name: 'Наименование товара',
-			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-			price: 10000
+		
+		// {
+		// 	name: 'Наименование товара',
+		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
+		// 	price: 10000
+		// },
+		// {
+		// 	name: 'Наименование товара',
+		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
+		// 	price: 10000
+		// },
+		// {
+		// 	name: 'Наименование товара',
+		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
+		// 	price: 10000
+		// },
+		// {
+		// 	name: 'Наименование товара',
+		// 	desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+		// 	img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
+		// 	price: 10000
+		// }
+		],
+		computed: {
+			sortedByMin: function() {
+			  function compare(a, b) {
+				if (a.price < b.price)
+				  return -1;
+				if (a.price > b.price)
+				  return 1;
+				return 0;
+			  }
+		
+			  return this.items.sort(compare);
+			},
+			sortedByMax: function() {
+				function compare(a, b) {
+				  if (a.price > b.price)
+					return -1;
+				  if (a.price < b.price)
+					return 1;
+				  return 0;
+				}
+		  
+				return this.items.sort(compare);
+			},
+			sortedByName: function() {
+				function compare(a, b) {
+				  if (a.name < b.name)
+					return -1;
+				  if (a.name > b.name)
+					return 1;
+				  return 0;
+				}
+		  
+				return this.items.sort(compare);
+			}
 		},
-		{
-			name: 'Наименование товара',
-			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-			price: 10000
-		},
-		{
-			name: 'Наименование товара',
-			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-			price: 10000
-		},
-		{
-			name: 'Наименование товара',
-			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-			price: 10000
-		},
-		{
-			name: 'Наименование товара',
-			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			img: 'https://www.bhphotovideo.com/images/images2500x2500/fujifilm_x100v_digital_camera_silver_1542675.jpg',
-			price: 10000
-		}]
+		sortOptions: [
+			{ value: 'default', text: 'По умолчанию' },
+			{ value: 'byMin', text: 'По цене min (от меньшего к большему)' },
+			{ value: 'byMax', text: 'По цене max (от большего к меньшему)' },
+			{ value: 'byName', text: 'По наименованию' } 
+		]
 	},
 
 	methods: {
@@ -66,7 +111,26 @@ new Vue({
 		},
 		removeItem(index) {
 			this.items.splice(index, 1)
+		},
+		sortItems() {
+			switch(this.sortBy) {
+				case 'byMin': 
+				  	return this.items.sort(byMin);
+				case 'byMax':
+				  	return this.items.sort(byMax);
+				case 'byName':
+					function compare(b, a) {
+						if (a.name > b.name) return -1;
+						return 0;
+					}
+				  	return this.items.sort(compare);
+				default:
+					return this.items;
+
+			}
 		}
 	}
 })
 
+byMin = (a, b) => parseFloat(a.price) - parseFloat(b.price);
+byMax = (b, a) => parseFloat(a.price) - parseFloat(b.price);
